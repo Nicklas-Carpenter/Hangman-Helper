5ab5c2cc149327a08a3e9552321f7d4acb05866e
Nicklas Carpenter <carpenter.nicklas@gmail.com>
2019-06-16 00:13:42 -0400

    Commented Python code, included license, updated README

--- README.md
+++ README.md
@@ -1,22 +1,41 @@
 # Hangman-Helper
+
 A solver for Hangman puzzles
 
+## Table of Contents
+
+[Installation](#Installation)
+
+[Usage](#Usage)
+
+[Contributing](#Contributing)
+
+[Credit](#Credit)
+
+[License](#License)
+
 ## Installation
-Installing the project is fairly simple. You can either clone the repository or download the ZIP file to obtain the project. No further installation steps are necessary. The program is currently implemented in a Python executable. It requires the user to have installed Python on their computer; the Python interpreter installed must be able to run Python 3. Python can be downloaded [here](https://www.python.org/).
+
+Installing the project is fairly simple. You can either clone the repository or download the ZIP file to obtain the project. The program is currently implemented in a Python executable. It requires the user to have installed Python on their computer; the Python interpreter installed must be able to run Python 3. Python can be downloaded [here](https://www.python.org/).
 
 ## Usage
-As long as the Python interpreter has been installed, all you need to do to run the program is click on the executable or run it from your favorite terminal. 
+
+To run the program, click on the executable or run it from the terminal.
 
 ## Contributing
-Currently, the project is not in a state where there is much room for contribution. Once the base functionality has been properly implemented and tested, long term plans include creating a UI component. At this stage, contribution guidelines will be established and contributions will be greatly appreciated.
+
+Currently, the project is not in a state where there is much room for contribution. Once the base functionality has been properly implemented and tested, long term plans include creating a UI component. At this stage, contribution guidelines will be established and contributions will be greatly appreciated. If you would like to contribute ahead of this, feel free to open a pull request.
 
 __If you encounter any bugs or issues, let me know by opening an issue in the [issue tab](https://github.com/Theseus001/Hangman-Helper/issues) on the Github page.__
+
 ## Credit
-The word list this program uses was obtained from [dwl/english-words](https://github.com/Theseus001/Hangman-Helper/issues). Thanks goes to dwl for compiling nearly 480 000 words into an easy-to-parse text document and for creating a second, more Hangman friendly subset of the list.
 
-Special thanks to the Dr. Claude Anderson and the Rose-Hulman Computer Science and Software Engineering Department for teaching me to write clean and effecient code and encouraging me to do so.
+The word list this program uses was obtained from [dwl/english-words](https://github.com/dwyl/english-words). Thanks goes to dwyl for compiling nearly 480 000 words into an easy-to-parse text document and for creating a second, more Hangman friendly subset of the list.
 
-Additionally, I would like to thank the [Python Software Foundation](https://www.python.org) and their team for writing clear, extensive, and easy-to-read [documentation for Python 3](https://docs.python.org/3/).
+Special thanks to Dr. Claude Anderson and the Rose-Hulman Computer Science and Software Engineering Department for teaching me to write clean and effecient code and encouraging me to do so.
+
+Additionally, I would like to thank the [Python Software Foundation](https://www.python.org) and their team for their extensive [documentation for Python 3](https://docs.python.org/3/).
 
 ## License
-This software is licensed under the GNU GPLv3. For more information see the LICENSE file.
+
+This software is licensed under the GNU GPLv3 or later. For more information see the LICENSE file.
--- hangman.py
+++ hangman.py
@@ -1,7 +1,32 @@
+'''
+A solver for Hangman puzzles
+Copyright (C) 2019  Nicklas Carpenter
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+'''
+
 import string
 
-class NLetterWord: 
+class NLetterWord:
+    """
+    Author: Nicklas Carpenter
+    Date Created: June 5, 2019
+    Date Last Modified: June 15, 2019
+    """
     def __init__(self, n_letters):
+        """An NLetterWord
+        """
         self.n_letters = n_letters
         self.word_list = []
         self.letters = []
@@ -15,8 +40,13 @@ class NLetterWord:
         self.init_word_list()
         self.update_letter_stats()
 
-
     def init_word_list(self):
+        """Initializes the list of potential words by reading from the master list and filtering by length
+
+        Date Created: June 5, 2019
+
+        Date Last Modified: June 15, 2019
+        """
         master_word_list = open('master_word_list')
         
         word = master_word_list.readline().rstrip('\n\r') #remove line endings
@@ -31,6 +61,13 @@ class NLetterWord:
             word_length = len(word) 
             
     def update_letter_stats(self):
+        """Updates the letter statistics by parsing throught the current word list
+        
+        Date Created: June 5, 2019
+
+        Date Last Modified: June 15, 2019
+        """
+
         for word in self.word_list:
             has_occured_in_word = {}
             for letter in word:
@@ -45,6 +82,16 @@ class NLetterWord:
             self.letter_stats[letter]['probability'] = self.letter_stats[letter]['word_occurrences'] / len(self.word_list)
 
     def handle_guess(self, letter, accepted):
+        """Calls for to the NLetterWord based on the guess and whether it was correct or not
+
+        Params:
+            letter - The letter guesed
+            accepted - Whether the letter guessed was correct or not
+            
+        Date Created: June 6, 2019
+
+        Date Last Modified: June 15, 2019
+        """
         self.letters.remove(letter)
         del self.letter_stats[letter]
 
@@ -57,31 +104,57 @@ class NLetterWord:
 
     
     def remove_words_containing(self, letter):
+        """Removes words from the word list that contain the specified letter
+
+        Params:
+            letter - The letter searched for
+
+        Date Created: June 6, 2019
+        Date Last Modified: June 15, 2019
+        """
         for word in self.word_list:
             if letter in word:
                 self.word_list.remove(word)
 
     
     def remove_words_not_containing(self, letter):
+        """Removes words from the word list that do not contain the specified letter
+
+        Params:
+            letter - The letter searched for
+
+        Date Created: June 6, 2019
+        Date Last Modified: June 15, 2019
+        """
         for word in self.word_list:
             if letter not in word:
                 self.word_list.remove(word)
 
 
     def print_word_list(self):
+        """Prints the current word list (potential solutions). Used for CLI.
+
+        Date Created: June 4, 2019
+        Date Last Modified: June 15, 2019
+        """
         for word in self.word_list:
             print(word)
 
     
     def print_letter_stats(self):
+        """Prints probability of a given letter appearing in the solution. Used for CLI.
+
+        Date Created: June 4, 2019
+        Date Last Modified: June 15, 2019
+        """
         for letter in self.letters:
             print(letter,': ', self.letter_stats[letter]['probability'])
 
-
+# Used for testing and CLI purposes
 if __name__ == '__main__':
     word_length = input("Word length: ")
-    game = NLetterWord(int(word_length))
+    word = NLetterWord(int(word_length))
     
     print("Letter Statistics:\n\n")
-    game.print_letter_stats()
+    word.print_letter_stats()
     
