040f39211c500e4029f94f8e61042fc9838b3ea5
Nicklas Carpenter <carpenter.nicklas@gmail.com>
2019-06-28 19:21:31 -0400

    Implemented frames.

--- hangman.py
+++ hangman.py
@@ -30,29 +30,34 @@ class NLetterWord:
     def __init__(self, n_letters):
         '''An NLetterWord'''
         self.n_letters = n_letters
-        self.word_list = set()
-        self.letters = []
+        self.solutions = set()
+        self.potential_letters = []
+        self.accepted_letters = set()
+        self.reject_letters = set()
         self.letter_stats = {}
         self.frame = n_letters * '_'
 
+        
+            
+        self.initialize()
+        self.update_letter_stats()
+
+
+    def initialize(self):
+        '''Initializes the list of potential words by reading from the master list and filtering by length.
+
+        Date Created: June 28, 2019
+
+        Date Last Modified: June 28, 2019
+        '''
+
         for letter in string.ascii_lowercase:
-            self.letters.append(letter)
+            self.potential_letters.append(letter)
             self.letter_stats[letter] = {
                 "word_occurrences": 0,
                 "occurrences": 0
             }
-            
-        self.init_word_list()
-        self.update_letter_stats()
-
-
-    def init_word_list(self):
-        '''Initializes the list of potential words by reading from the master list and filtering by length.
-
-        Date Created: June 5, 2019
-
-        Date Last Modified: June 15, 2019
-        '''
+        
         master_word_list = open('master_word_list')
         
         word = master_word_list.readline().rstrip(' \n\r') # remove line endings
@@ -61,7 +66,7 @@ class NLetterWord:
         while word_length > 0:
 
             if word_length == self.n_letters:
-                self.word_list.add(word)
+                self.solutions.add(word)
 
             word = master_word_list.readline().rstrip('\n\r')
             word_length = len(word) 
@@ -77,12 +82,12 @@ class NLetterWord:
 
         # Reset the count for each letter before we recount.
 
-        for letter in self.letters:
+        for letter in self.potential_letters:
             self.letter_stats[letter]['occurrences'] = 0
             self.letter_stats[letter]['word_occurrences'] = 0
 
         # Count how many words each letter appears in as well as how many total times the letter appears in the list.
-        for word in self.word_list:
+        for word in self.solutions:
             has_occured_in_word = set()
             for letter in word:
                 if letter not in has_occured_in_word:
@@ -91,60 +96,97 @@ class NLetterWord:
                 self.letter_stats[letter]['occurrences'] += 1
 
         # Sort letter list in order of occurences (descending).  
-        self.letters.sort(key = lambda letter: self.letter_stats[letter]['word_occurrences'], reverse = True)
+        self.potential_letters.sort(key = lambda letter: self.letter_stats[letter]['word_occurrences'], reverse = True)
         
         # Calculate the probability a letter is in a given word.
-        for letter in self.letters:
-            self.letter_stats[letter]['probability'] = self.letter_stats[letter]['word_occurrences'] / len(self.word_list)
-
-
-    def handle_guess(self, letter, accepted):
-        '''Updates the possible letters, wordlist, and letter stats based on a given guess.
-        
-        Date Created: June 5, 2019
-
-        Date Last Modified: June 15, 2019
-        '''
-        
-        # Remove the letter even if it is correct. If it is an incorrect guess we know that the probability of it
-        # occuring is 0. If it is a correct guess, we know the probability of it occuring is 1. Either way, we don't
-        # care about the letter's statistics.
-        self.letters.remove(letter)
-
-        # Sort letter list in order of occurences (descending).
-        self.letters.sort(key = lambda letter: self.letter_stats[letter]['word_occurrences'], reverse = True)
-        
-        if accepted:
-            self.remove_words_if_not_contains(letter)
-        else:
-            self.remove_words_if_contains(letter)
-
-        self.update_letter_stats()
+        for letter in self.potential_letters:
+            if self.letter_stats[letter]['occurrences'] == 0:
+                self.reject_letters.add(letter)
+                self.potential_letters.remove(letter)
+            self.letter_stats[letter]['probability'] = self.letter_stats[letter]['word_occurrences'] / len(self.solutions)
 
 
     def accept(self, letter, positions):
-        ''''''
-        pass
+        '''Handles a correct letter guess.
 
+        Created: June 26, 2019
+        Last Updated: June 28, 2019
+        '''
 
-    def filter_by_frame(self, letter, positions):
-        ''''''
-
-        regex = list('.' * self.n_letters)
-        for i in range(len(regex)):
-            regex[i] = letter
-        regex = ''.join(regex)
-        regex = re.compile(regex)
-
-        updated_word_list = set()
-
-        for word in self.word_list:
-            if regex.fullmatch(word):
-                updated_word_list.add(word)
+        self.potential_letters.remove(letter)
+        self.accepted_letters.add(letter)
+        self.update_frame(letter, positions)
+        self.filter_by_frame()
+        self.update_letter_stats()
+    
+    def reject(self, letter):
+        '''Handles and incorrect letter guess
         
-        self.word_list = updated_word_list
+        Created: June 28, 2019
+        Last Updated: June 28, 2019
+        '''
 
-    def remove_words_if_contains(self, letter):
+        self.potential_letters.remove(letter)
+        self.reject_letters.add(letter)
+        self.filter_by_contains(letter)
+        self.update_letter_stats()
+
+
+    def update_frame(self, letter, positions):
+        '''Updates the frame by puttint the given letter in the specified positions. 
+        
+            To remove or reposition letters, the frame must be updated and therefore reconstructed.
+
+            Created: June 28, 2019
+
+            Last Updated: June 28, 2019
+        '''
+        
+        for position in positions:
+            index = int(position) -1 # Convert position (which starts at 1) to index (which starts at 0).
+            self.frame = self.frame[0 : int(index )] + letter + self.frame[int(index) + 1: len(self.frame)]
+
+
+    def set_state(self, frame, rejected):
+        self.initialize()
+
+        self.frame = frame
+
+        for letter in frame.replace('_',''):
+            if letter not in self.accepted_letters:
+                self.accepted_letters.add(letter)
+                self.potential_letters.remove(letter)
+        
+        for letter in rejected:
+            self.reject_letters.add(letter)
+            self.potential_letters.remove(letter)
+
+        self.filter_by_frame()
+
+        self.update_letter_stats()
+
+    
+    def filter_by_frame(self):
+        '''Filters out words that do not match the letters and positions in the frame
+
+        Created: June 26, 2019
+
+        Last Updated: June 28, 2019
+        '''
+
+        regex = re.compile(self.frame.replace('_','.'))
+
+        updated_solutions = set()
+
+        for word in self.solutions:
+            if regex.fullmatch(word):
+                updated_solutions.add(word)
+                print("Added: ", word)
+        
+        self.solutions = updated_solutions
+
+
+    def filter_by_contains(self, letter):
         '''Removes words from the word list if they contain the given letter.
 
         Params:
@@ -157,31 +199,11 @@ class NLetterWord:
 
         updated_word_list = set()
 
-        for word in self.word_list:
+        for word in self.solutions:
             if letter not in word:
                 updated_word_list.add(word)
         
-        self.word_list = updated_word_list
-
-        
-    def remove_words_if_not_contains(self, letter):
-        '''Removes words if they do not contain the given letter.
-
-        Params:
-            letter - The letter searched for
-
-        Date Created: June 6, 2019
-
-        Date Last Modified: June 23, 2019
-        '''
-        
-        updated_word_list = set()
-
-        for word in self.word_list:
-            if letter in word:
-                updated_word_list.add(word)
-        
-        self.word_list = updated_word_list
+        self.solutions = updated_word_list
         
 
     def print_word_list(self):
@@ -191,7 +213,7 @@ class NLetterWord:
 
         Date Last Modified: June 15, 2019
         '''
-        for word in self.word_list:
+        for word in self.solutions:
             print(word,'\n')
 
     
@@ -201,7 +223,7 @@ class NLetterWord:
         Date Created: June 4, 2019
         Date Last Modified: June 15, 2019
         '''
-        for letter in self.letters:
+        for letter in self.potential_letters:
             print(letter,': ', self.letter_stats[letter]['probability'])
     
 
@@ -213,7 +235,7 @@ class NLetterWord:
         Date Last Modified: June 19, 2019
         '''
 
-        return len(self.word_list)
+        return len(self.solutions)
 
 
 # Used for testing and CLI purposes
@@ -224,22 +246,26 @@ if __name__ == '__main__':
     
     # Main loop
     while True:
-        command_str = input('>')
+        command_str = input('>').lower()
         args = command_str.split(sep = ' ')
         
         if args[0] == 'accept':
-            if len(args) > 2:
+            if len(args) < 3:
                 # print(len(args))
                 # print(args)
-                print('Usage: accept [LETTER]')
+                print('Usage: accept [LETTER] [POSITION]...')
                 continue
-            word.handle_guess(args[1], True)
+            word.accept(args[1], args[2 : len(args)])
             
         elif args[0] == 'reject':
             if len(args) > 2:
                 print('\nUsage: reject [LETTER]\n')
                 continue
-            word.handle_guess(args[1], False)
+            word.reject(args[1])
+
+        elif args[0] == 'set':
+            if len(args) > 2:
+                print('\nUsage: set [FRAME]\n')
 
         elif args[0] == 'stats':
             if len(args) > 1:
@@ -263,8 +289,14 @@ if __name__ == '__main__':
                 continue
             print('\n',word.solutions_count(), 'possible solutions\n')
 
-        elif args[0] == 'q':
+        elif args[0] == 'status':
+            if len(args) > 1:
+                print('\nUsage: status\n')
+                continue
+            print('\nCurrent: ', word.frame,'\n')
+
+        elif args[0] == 'quit':
             exit()
 
         else:
-            print('\nUsage:\n\taccept [LETTER]\n\treject [LETTER]\n\tstats\n\twords\n\tcount')
+            print('\nUsage:\n\taccept [LETTER]\n\treject [LETTER]\n\tstats\n\twords\n\tcount\n\tstatus')
